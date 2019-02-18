13677cad3de542a39366d3fc02ec1e98672f67db
Light <lsycxyj@gmail.com>
2019-02-19 00:05:07 +0800

    feature/v1.1.0 add test cases change karma config backup

--- test/unit/karma.conf.js
+++ test/unit/karma.conf.js
@@ -10,7 +10,11 @@ const projectRoot = path.resolve(__dirname, '../../');
 
 const webpackConfig = merge(baseConfig, {
 	// use inline sourcemap for karma-sourcemap-loader
-	devtool: '#inline-source-map',
+	devtool: 'inline-cheap-module-source-map',
+	output: {
+		devtoolModuleFilenameTemplate: '[absolute-resource-path]',
+		devtoolFallbackModuleFilenameTemplate: '[absolute-resource-path]?[hash]',
+	},
 });
 
 // no need for app entry during tests
@@ -33,7 +37,9 @@ process.env.CHROME_BIN = require('puppeteer').executablePath();
 
 const reporters = ['mocha'];
 if (process.env.USE_COVERAGE) {
-	reporters.push('coverage');
+	reporters.push('coverage-istanbul');
+	// reporters.push('coverage');
+	// reporters.push('karma-remap-istanbul');
 }
 
 module.exports = function (config) {
@@ -76,17 +82,69 @@ module.exports = function (config) {
 		webpackMiddleware: {
 			noInfo: true,
 		},
+
 		// Coverage options
-		coverageReporter: {
-			dir: './coverage',
-			reporters: [
-				// There is a bug of html reporter to write files when I try to use single file component of vue
-				// { type: 'html', subdir: '.' },
-				{ type: 'text', file: 'text.txt' },
-				{ type: 'lcovonly', subdir: '.' },
-				{ type: 'text-summary' },
+		// coverageReporter: {
+		// 	dir: './coverage',
+		// 	reporters: [
+		// 		// There is a bug of html reporter to write files because of wrong file names when I try to use single file component of vue
+		// 		// { type: 'html', subdir: '.' },
+		// 		{ type: 'text', file: 'text.txt' },
+		// 		{ type: 'lcovonly', subdir: '.' },
+		// 		{ type: 'text-summary' },
+		// 	],
+		// },
+
+		// any of these options are valid: https://github.com/istanbuljs/istanbuljs/blob/aae256fb8b9a3d19414dcf069c592e88712c32c6/packages/istanbul-api/lib/config.js#L33-L39
+		coverageIstanbulReporter: {
+			// reports can be any that are listed here: https://github.com/istanbuljs/istanbuljs/tree/aae256fb8b9a3d19414dcf069c592e88712c32c6/packages/istanbul-reports/lib
+			reports: [
+				// 'html',
+				'text',
+				'lcovonly',
+				'text-summary',
 			],
+
+			// base output directory. If you include %browser% in the path it will be replaced with the karma browser name
+			dir: path.join(__dirname, 'coverage'),
+
+			// Combines coverage information from multiple browsers into one report rather than outputting a report
+			// for each browser.
+			combineBrowserReports: true,
+
+			// if using webpack and pre-loaders, work around webpack breaking the source path
+			fixWebpackSourcePaths: true,
+
+			// Omit files with no statements, no functions and no branches from the report
+			skipFilesWithNoCoverage: true,
+
+			// Most reporters accept additional config options. You can pass these through the `report-config` option
+			'report-config': {
+				// all options available at: https://github.com/istanbuljs/istanbuljs/blob/aae256fb8b9a3d19414dcf069c592e88712c32c6/packages/istanbul-reports/lib/html/index.js#L135-L137
+				html: {
+					// outputs the report in ./coverage/html
+					subdir: 'html',
+				},
+				text: {
+					file: './coverage.txt',
+				},
+			},
+
+			verbose: true, // output config used by istanbul for debugging
 		},
+
+		// remapIstanbulReporter: {
+		//   remapOptions: {
+		//     mapFileName(filename) {
+		//       const originalFilename = filename.replace(/\.vue(\.[jt]s)?(\?.+)?$/, '.vue');
+		//       console.log(originalFilename);
+		//     	return originalFilename;
+		// 		},
+		// 	},
+		//   reports: {
+		//     html: 'coverage',
+		//   },
+		// },
 
 		// web server port
 		port: 9876,
